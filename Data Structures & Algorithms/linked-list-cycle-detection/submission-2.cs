fb6fed2e34f9e5582d5c9809eb632254dd6535d2
Peter Collins <131217467+psc3245@users.noreply.github.com>
2026-08-18 13:24:25 -0500

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.cs
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        HashSet<ListNode> seen = new HashSet<ListNode>();
+        var curr = head;
+        while (curr != null) {
+            if (!seen.Add(curr)) {
+                return true;
+            }
+            curr = curr.next;
+        }
+        return false;
+    }
+}
